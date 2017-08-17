c3770aafe79ee4fa7dbb758a6453c48591ed2cbf
imolorhe <samuelimolo4real@gmail.com>
2017-08-17 16:11:09 +0100

    Added configs table. Changed data types in Mapper to more appropriate types. Mapped Lagos State.

--- src/main/scala/fourwords/Main.scala
+++ src/main/scala/fourwords/Main.scala
@@ -9,6 +9,14 @@ import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 
+class Configs(tag: Tag) extends Table[(Int, String, String)](tag, "configs") {
+  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  def configKey = column[String]("config_key")
+  def configValue = column[String]("config_value")
+
+  def * = (id, configKey, configValue)
+}
+
 class Mappings(tag: Tag) extends Table[(Int, Float, Float, String)](tag, "mappings") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def pointX = column[Float]("point_x")
@@ -24,23 +32,70 @@ object Main extends App{
   Console println "***** FOURWORDS *****"
 
   val mappings = TableQuery[Mappings]
+  val configs = TableQuery[Configs]
   val setup = DBIO.seq(
-    mappings.schema.create
+    (configs.schema ++ mappings.schema).create
   )
 
-  val drop = DBIO.seq(
+  val dropMappings = DBIO.seq(
     mappings.schema.drop
+  )
+  val dropConfigs = DBIO.seq(
+    configs.schema.drop
   )
 
   val db = Database.forConfig("db")
 
   // Check if the mappings table already exists, and drop it before running setup to create it
-  val existingTables = Await.result(db.run(MTable.getTables("mappings")), Duration.Inf).toList
-  if(existingTables.nonEmpty) Await.result(db.run(drop), Duration.Inf)
+  val existingMappingTables = Await.result(db.run(MTable.getTables("mappings")), Duration.Inf).toList
+  if(existingMappingTables.nonEmpty) Await.result(db.run(dropMappings), Duration.Inf)
+
+  // Check if the configs table already exists, and drop it before running setup to create it
+  val existingConfigTables = Await.result(db.run(MTable.getTables("configs")), Duration.Inf).toList
+  if(existingConfigTables.nonEmpty) Await.result(db.run(dropConfigs), Duration.Inf)
 
   Await.result(db.run(setup), Duration.Inf)
 
-  val mapper = new Mapper()
+  // Set the mapping config
+//  val mappingConfig = Map[String, Double](
+//    "origin_x" -> 0,
+//    "origin_y" -> 0,
+//    "region_width" -> 100.0,
+//    "region_height" -> 100,
+//    "unit_width" -> 5,
+//    "unit_height" -> 5
+//  )
+
+  // Setting parameters based on Lagos' location
+  val startX = 2.71199
+  val endX = 4.34997
+  val startY = -6.70552
+  val endY = -6.37569
+  val unitWidth = 0.001
+  val unitHeight = 0.001
+  val mappingConfig = Map[String, Double](
+    "origin_x" -> startX,
+    "origin_y" -> startY,
+    "region_width" -> (endX - startX).abs,
+    "region_height" -> (endY - startY).abs,
+    "unit_width" -> unitWidth,
+    "unit_height" -> unitHeight
+  )
+  val mapper = new Mapper(
+    x = mappingConfig("origin_x"),
+    y = mappingConfig("origin_y"),
+    w = mappingConfig("region_width"),
+    h = mappingConfig("region_height"),
+    uw = mappingConfig("unit_width"),
+    uh = mappingConfig("unit_height")
+  )
+
+  println("Store configs:")
+  val storeConfigs = DBIO.seq(
+    configs ++= mappingConfig.toSeq.map(v => (0, v._1, v._2.toString))
+  )
+  Await.result(db.run(storeConfigs), Duration.Inf)
+  println("Configs stored.")
 
   println("Store mappings:")
   val storeMappings = DBIO.seq(
--- src/main/scala/fourwords/Mapper.scala
+++ src/main/scala/fourwords/Mapper.scala
@@ -4,24 +4,25 @@ import scala.collection.mutable
 
 /**
   * Maps the provided area and unit area to generated fourwords
-  *
-  * @param x
-  * @param y
-  * @param w
-  * @param h
+  * @param x the origin x
+  * @param y the origin y
+  * @param w the width of the entire region
+  * @param h the height of the entire region
+  * @param uw the unit width of a mapping
+  * @param uh the unit height of a mapping
   */
-class Mapper(x: Int = 0, y: Int = 0, w: Int = 100, h: Int = 100, uw: Int = 5, uh: Int = 5) {
-  val num_rows: Int = h / uh
-  val num_cols: Int = w / uw
+class Mapper(x: Double = 0, y: Double = 0, w: Double = 100, h: Double = 100, uw: Double = 5, uh: Double = 5) {
+  val num_rows: Long = (h / uh).round
+  val num_cols: Long = (w / uw).round
 
   // Stores the mappings
-  var map: mutable.HashMap[(String, String, String, String), (Int, Int)] = new mutable.HashMap[(String, String, String, String), (Int, Int)]()
+  var map: mutable.HashMap[(String, String, String, String), (Double, Double)] = new mutable.HashMap[(String, String, String, String), (Double, Double)]()
 
   // i specifies the current row (y-axis)
-  for(i <- 0 until num_rows){
+  for(i <- 0 until num_rows.toInt){
     // j specifies the current column (x-axis)
-    for(j <- 0 until num_cols){
-      var curEntry = (WordsGenerator.getWordX, (uw * j, uh * i))
+    for(j <- 0 until num_cols.toInt){
+      var curEntry = (WordsGenerator.getWordX, (x + (uw * j), y + (uh * i)))
       map += curEntry
     }
   }
@@ -30,16 +31,16 @@ class Mapper(x: Int = 0, y: Int = 0, w: Int = 100, h: Int = 100, uw: Int = 5, uh
   println(s"Total mappings provided: ${map.size}")
   println(s"Total mappings possible: ${WordsGenerator.maxWordX}")
 
-  def searchWordX(wordX: (String, String, String, String)): Option[((String, String, String, String), (Int, Int))] = map.find(_._1 == wordX)
+  def searchWordX(wordX: (String, String, String, String)): Option[((String, String, String, String), (Double, Double))] = map.find(_._1 == wordX)
 
-  def searchPoint(point: (Int, Int)): Option[((String, String, String, String), (Int, Int))] = {
+  def searchPoint(point: (Double, Double)): Option[((String, String, String, String), (Double, Double))] = {
     // Subtract the modulo to get the point which was used in mapping
     // Round down to the nearest mapped point
     val curPoint = (point._1 - point._1 % uw, point._2 - point._2 % uh)
     map.find(_._2 == curPoint)
   }
 
-  def humanFriendly(mapItem: Option[((String, String, String, String), (Int, Int))]): String = {
+  def humanFriendly(mapItem: Option[((String, String, String, String), (Double, Double))]): String = {
     mapItem match {
       case Some(s) => s._1.productIterator.mkString(".")
       case None => ""
@@ -50,5 +51,5 @@ class Mapper(x: Int = 0, y: Int = 0, w: Int = 100, h: Int = 100, uw: Int = 5, uh
     (0, v._2._1.toFloat, v._2._2.toFloat, humanFriendly(Option(v)))
   }).toSeq
 
-  def getMappings: mutable.HashMap[(String, String, String, String), (Int, Int)] = map
+  def getMappings: mutable.HashMap[(String, String, String, String), (Double, Double)] = map
 }
